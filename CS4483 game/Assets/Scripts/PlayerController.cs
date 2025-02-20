542f33df254b42a876d989dc21faa204f399ae0a
Ann <ann0422xyx@gmail.com>
2025-02-20 15:37:39 -0500

    shading, environmental light?

--- CS4483 game/Assets/Scripts/PlayerController.cs
+++ CS4483 game/Assets/Scripts/PlayerController.cs
@@ -1,81 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    
-    private Rigidbody2D rb;
-
-    // Player Status
-    public float moveSpeed = 3f;
-    private float originalMoveSpeed;
-    public int power;
-    public int maxHp;
-    public int CurHp;
-    public float maxStamina;
-    public float curStamina;
-    private float staminaBoost = 0.1f;
-    private Vector3 originalPosition;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        originalMoveSpeed = moveSpeed;
-        CurHp = maxHp;
-        curStamina = maxStamina;
-        power = 1;
-        originalPosition = transform.position;
-    }
+        public float speed;
 
-    void Update()
-    {
-        HandleMovement();
-        if (CurHp <= 0)
+        // private Animator animator;
+
+        private void Start()
         {
-            rb.linearVelocity = Vector2.zero;
+            // animator = GetComponent<Animator>();
         }
 
-    }
 
-    public void HandleMovement()
-    {
-
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        Vector2 moveDir = new Vector2(moveX, moveY).normalized;
-
-
-
-        if (moveDir != Vector2.zero)
+        private void Update()
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            Vector2 dir = Vector2.zero;
+            if (Input.GetKey(KeyCode.A))
             {
-                if (curStamina < 0)
-                {
-                    rb.linearVelocity = moveDir * moveSpeed;
-
-                }
-                else
-                {
-                    rb.linearVelocity = moveDir * moveSpeed * 1.5f;
-                    curStamina -= 0.2f;
-                }
+                dir.x = -1;
+                // animator.SetInteger("Direction", 3);
             }
-            else
+            else if (Input.GetKey(KeyCode.D))
             {
-
-                rb.linearVelocity = moveDir * moveSpeed;
-                if (curStamina < 100)
-                {
-                    curStamina += staminaBoost;
-                }
-
+                dir.x = 1;
+                // animator.SetInteger("Direction", 2);
             }
 
-        }
-        else
-        {
-            rb.linearVelocity = Vector2.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.y = 1;
+                // animator.SetInteger("Direction", 1);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+                // animator.SetInteger("Direction", 0);
+            }
+
+            dir.Normalize();
+            // animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            GetComponent<Rigidbody2D>().linearVelocity = speed * dir;
         }
     }
-}
+
