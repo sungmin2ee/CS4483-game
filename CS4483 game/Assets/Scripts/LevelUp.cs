e17716a63c5aa6fdb33f607ebea01782e780cc41
sungmin2ee <tjdalsjang@gmail.com>
2025-03-16 14:47:59 -0400

    level up

--- CS4483 game/Assets/Scripts/LevelUp.cs
+++ CS4483 game/Assets/Scripts/LevelUp.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class LevelUp : MonoBehaviour
 {
     RectTransform rect;
+    Item[] items;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rect = GetComponent<RectTransform>();
+        items = GetComponentsInChildren<Item>(true);
     }
 
     // Update is called once per frame
@@ -20,5 +22,9 @@ public class LevelUp : MonoBehaviour
         rect.localScale = Vector3.zero;
         Time.timeScale = 1;
 
+    }
+    public void Select(int i)
+    {
+        items[i].Onclick();
     }
 }
