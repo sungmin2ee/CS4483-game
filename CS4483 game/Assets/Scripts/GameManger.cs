4fa4827749326f5fbe0b5f4f8f59aa629df59b8b
sungmin2ee <tjdalsjang@gmail.com>
2025-03-16 14:21:57 -0400

    level up

--- CS4483 game/Assets/Scripts/GameManger.cs
+++ CS4483 game/Assets/Scripts/GameManger.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        // ½Ì±ÛÅæ ÆĞÅÏ Àû¿ë
+       
         if (Instance == null)
         {
             Instance = this;
